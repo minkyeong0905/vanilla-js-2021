925a783c2c0dfa32f39e1551eaf5c4dc05720bc2
minkyeong0905 <mingit0905@naver.com>
2025-02-23 20:38:24 +0900

    Modified style.css and index.html & background.js

--- js/background.js
+++ js/background.js
@@ -2,9 +2,15 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+//const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImage}`;
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+//document.body.appendChild(bgImage);
 //document.body.prepend(bgImage);
+
+const imgUrl = `img/${chosenImage}`;
+document.body.style.backgroundImage = `url(${imgUrl})`;
+document.body.style.backgroundPosition = "top";
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundAttachment = "fixed";
